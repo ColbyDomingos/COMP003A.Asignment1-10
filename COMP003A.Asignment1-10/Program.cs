62622ceeb4af9f9c8fe94a5fdd107a64a0e4b8b9
Colby Domingos <colbydomingos@my.whccd.edu>
2025-01-15 20:49:46 -0800

    Add project files.

---
+++ COMP003A.Asignment1-10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace COMP003A.Asignment1_10
+{
+    internal class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Type your name: ");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Hi " + username + ". My name is Colby");
+
+            Console.WriteLine("Here are 10 things about me: ");
+
+            Console.WriteLine("\t1. I like food ");
+            Console.WriteLine("\t2. I like video games ");
+            Console.WriteLine("\t3. I like sports ");
+            Console.WriteLine("\t4. I like Asian food the most ");
+            Console.WriteLine("\t5. I like Animals ");
+            Console.WriteLine("\t6. I like Music and making it ");
+            Console.WriteLine("\t7. I like going to school ");
+            Console.WriteLine("\t8. I like talking to new people ");
+            Console.WriteLine("\t9. I like conversing with my teachers ");
+            Console.WriteLine("\t10. I like experiencig the world at face value ");
+
+        }
+    }
+}
